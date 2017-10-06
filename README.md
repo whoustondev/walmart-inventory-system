f427aaf5128d9f25f29c0d51dcd639bf5723b2f3
Will Houston <whouston@Wills-MacBook-Pro.local>
2017-10-05 22:05:34 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This is for a group project at George Mason University. 
+
+This is all public domain, seeing as GMU is a public research university. 
+
+
---
+++ test.java
@@ -0,0 +1,13 @@
+
+
+
+public class test
+{
+	public static void main(String[] args)
+	{
+		
+		System.out.println("what is up, is this working for you ?");
+		
+		
+	}
+}
