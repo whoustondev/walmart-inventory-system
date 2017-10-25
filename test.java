7524fc37f1b3de2b7e7c51b4e72153a392e87aac
Will Houston <whouston@Wills-MBP.byod.gmu.edu>
2017-10-24 20:47:58 -0400

    test.java is a good jumping off point for this project. It contains code related to joptionpane, and a good start to the username/password part of our project.

--- test.java
+++ test.java
@@ -1,13 +1,40 @@
 
+import javax.swing.*;
+import java.awt.*;
 
 
 public class test
 {
 	public static void main(String[] args)
 	{
+
 		
-		System.out.println("what is up, is this working for you ?");
-		
-		
+	    Object[] options1 = { "Login", "Cancel"};
+
+	    	JPanel panel = new JPanel();
+	    	panel.setBackground(Color.lightGray);
+	    	JTextField username = new JTextField(10);
+	    	JTextField password = new JTextField(10);
+	    	panel.add(Box.createVerticalStrut(20)); // a spacer
+	    	panel.add(new Label("Username:"));
+	    	panel.add(username);
+	    
+	    	panel.add(new Label("Password:"));
+	    	
+	    	panel.add(password);
+	    	int result = JOptionPane.showOptionDialog(null, panel, "Login Page", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, options1, null);
+	    	if (result == JOptionPane.YES_OPTION)
+	    	{
+	    		JOptionPane.showMessageDialog(null, username.getText());
+	    	
+	    	}
+	    	else 
+	    	{
+	    		System.exit(0);
+	    		
+	    	}
+	    	System.exit(0);
+
 	}
+		
 }
