1373b5138121e6755946055db72a7ae6dc506604
Will Houston <whoustondev@gmail.com>
2017-11-30 21:21:36 -0500

    added Login.java

--- Driver.java
+++ Driver.java
@@ -7,38 +7,6 @@ public class Driver {
 	public static void main (String[] args) throws IOException
 	{
 		WalmartSystem system = new WalmartSystem();
-		system.run();
-//		Book a = new Book("A good time", "will Houston","Comedy", 34);
-//		Toy b = new Toy("Fisher price", 23,2);
-//		Clothing c = new Clothing("Tshirt", "Polo","CBlue", 34, 34,32,12);
-//		system.addItemToDatabase("./IT-306WalmartInventorySystem/data.txt", a );
-//		system.addItemToDatabase("./IT-306WalmartInventorySystem/data.txt", b );
-//		system.addItemToDatabase("./IT-306WalmartInventorySystem/data.txt", c );
-////
-//		system.run();
-//		Book a = new Book("Z Brave New World", "Alduous Huxley","Scary", 35);
-//		
-//		Book b = new Book("GGGGG", "George Orwell","Scary", 22);
-//		Book c = new Book("AAAAAHillary goes to jail", "Hillary Clinton","LoserBooks", 59);
-//		
-//		system.books.add(a);
-//		system.books.add(b);
-//		system.books.add(c);
-//		System.out.println("before");
-//		for (int i = 0; i< system.books.size(); i++)
-//		{	
-//			if(system.books.get(i) instanceof Book)
-//				System.out.println(((Book)system.books.get(i)).getQuantity());		
-//		}
-//		LinkedList ha = WalmartSystem.sort(system.books, false, "Quantity");
-//		System.out.println("After");
-//		for (int i = 0; i<ha.size(); i++)
-//		{	if(ha.get(i) instanceof Book)
-//				System.out.println(((Book)ha.get(i)).getQuantity());		
-//		}
-		
-		
-		
-		
+		system.run();		
 	}
 }
